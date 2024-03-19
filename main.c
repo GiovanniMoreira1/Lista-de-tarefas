928e38a18a2c851a35c32c4370dcffab918162c8
GiovanniMoreira1 <giovannimoreira131@gmail.com>
2024-03-19 22:31:38 +0000

    primeiro commit

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  printf("faz o l\n");
+  printf("Ariovaldo bolsonaro\n");
+  printf("Ariovaldo bolsonaro\n");
   return 0;
 }
+
+// 
